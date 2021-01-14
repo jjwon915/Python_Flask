dedb8ba2716f0d9efd8865bc92c44ebce2958902
jjwon915 <jjwon915@naver.com>
2021-01-15 00:22:24 +0900

    크롤링 하도록 함수를 불러오고, index.html로 받아온 값을 넘겨준다.

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ import crawling
 @app.route('/')
 def hello():
 
-    list_daum = crawling.daum_news()
-    list_today = crawling.today()
-    list_okky = crawling.okky()
-
+    list_daum_news, list_daum_news_href = crawling.daum_news()
+    list_today, list_today_href = crawling.today()
+    list_okky, list_okky_href = crawling.okky()
 
     return render_template("index.html",
-                           daum_news = list_daum,
+                           daum_news = list_daum_news,
                            today = list_today,
-                           okky = list_okky)
+                           okky = list_okky,
+                           daum_href = list_daum_news_href,
+                           daum_len = len(list_daum_news),
+                           today_href = list_today_href,
+                           today_len = len(list_today),
+                           okky_len = len(list_okky),
+                           okky_href = list_okky_href)
 
 @app.route('/about')
 def about():
