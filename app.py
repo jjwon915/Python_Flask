0798b41b3c062b04b84bddd6af74b7c41df75335
jjwon915 <jjwon915@naver.com>
2021-01-14 22:01:52 +0900

    1. https://news.daum.net/ranking/popular는 뉴스 랭킹 목록 가져오기 위한 사이트이다. 2. 위의 사이트에서 li > div.cont_thumb > strong 태그 안의 a태그에 뉴스 제목 text가 위치해 경로를 써주었다. 3. list_daum에 a 태그 안의 text를 추가한다. 4. index.html에서 볼 수 있도록 list_daum을 보낸다.

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# 크롤링 라이브러리 import
+import requests
+from bs4 import BeautifulSoup
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    # https://news.daum.net/ranking/popular 에서 뉴스 랭킹 목록 가져오기 위함.
+    req = requests.get("https://news.daum.net/ranking/popular")
+
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    list_daum = []
+
+    for i in soup.select("#mArticle > div.rank_news > ul.list_news2 > li > div.cont_thumb > strong"):
+        list_daum.append(i.find("a").text)
+
+    return render_template("index.html", daum = list_daum)
 
 @app.route('/about')
 def about():
