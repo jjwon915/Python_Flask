70fa3ba148df90ad307af5a4de48a2837707abaa
jjwon915 <jjwon915@naver.com>
2021-01-15 00:21:24 +0900

    글의 제목 뿐만아니라 글의 링크까지 크롤링해서 반환하도록 했다.

--- crawling.py
+++ crawling.py
@@ -9,12 +9,14 @@ def daum_news():
 
     soup = BeautifulSoup(req.text, 'html.parser')
 
-    list_daum = []
+    list_daum_news = []
+    list_daum_news_href =[]
 
     for i in soup.select("#mArticle > div.rank_news > ul.list_news2 > li > div.cont_thumb > strong"):
-        list_daum.append(i.find("a").text)
+        list_daum_news.append(i.find("a").text)
+        list_daum_news_href.append(i.find("a")["href"])
 
-    return list_daum
+    return list_daum_news, list_daum_news_href
 
 # 오늘의 유머
 def today():
@@ -23,11 +25,13 @@ def today():
     soup = BeautifulSoup(req.text, 'html.parser')
 
     list_today = []
+    list_today_href = []
 
-    for i in soup.find_all("td", class_="subject") :
+    for i in soup.find_all("td", class_="subject"):
         list_today.append(i.text)
+        list_today_href.append("http://www.todayhumor.co.kr" + i.find("a")["href"])
 
-    return list_today
+    return list_today, list_today_href
 
 # OKKY
 def okky():
@@ -36,8 +40,10 @@ def okky():
     soup = BeautifulSoup(req.text, 'html.parser')
 
     list_okky = []
+    list_okky_href = []
 
     for i in soup.select("#list-article > div.panel.panel-default > ul > li > div.list-title-wrapper.clearfix > h5"):
         list_okky.append(i.find("a").text)
+        list_okky_href.append("https://okky.kr" + i.find("a")["href"])
 
-    return list_okky
+    return list_okky, list_okky_href
