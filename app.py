8aacda5dadca6066c5af4107339dbf8a1dd32ae3
jjwon915 <jjwon915@naver.com>
2021-01-14 18:00:15 +0900

    flask_기초_테스트

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return "여기는 about입니다."
 
 if __name__ == '__main__':
     app.run()
